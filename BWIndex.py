b81786dc56d309e596ba553a551d816d374bd7ee
Gretchen Wright <gretchen.m.wright@gmail.com>
2019-02-15 16:31:21 -0500

    first installment of capitalization changes

--- BWIndex.py
+++ BWIndex.py
@@ -1,68 +1,70 @@
+import argparse
+import sys
+
 from graphviz import Digraph
-import sys, argparse
 
 
-class BWIndex():
+class BWIndex:
 	def __init__(self, genome=None, genomefile=None):
 		if genome is not None:
 			self.Text = genome
 		elif genomefile is not None:
-			self.loadGenomeFromFile(genomefile)
+			self.load_genome_from_file(genomefile)
 		else:
 			print("You must supply a genome file or a genome string.")
 			sys.exit()
 		if self.Text[-1] != '$':
 			print("Genome must be terminated by $")
 			sys.exit()
-		self.buildTree()
+		self.build_tree()
 		self.Solve()
 		self.Count = dict()
 		self.letters = {i for i in self.BWT}
 		self.alphabet = [i for i in self.letters]
 		self.alphabet.sort()
-		self.computeCountArray()
+		self.compute_count_array()
 
-		self.FirstOccurrence = dict()
+		self.first_occurrence = dict()
 		freq = dict()
 		for i in self.BWT:
 			freq[i] = freq.get(i, 0) + 1
 		index = 0
-		self.FirstOccurrence['$'] = 0
+		self.first_occurrence['$'] = 0
 		for j in range(1, len(self.alphabet)):
-			prevLetter = self.alphabet[j - 1]
+			prev_letter = self.alphabet[j - 1]
 			letter = self.alphabet[j]
-			index += freq[prevLetter]
-			self.FirstOccurrence[letter] = index
+			index += freq[prev_letter]
+			self.first_occurrence[letter] = index
 
-	def buildTree(self):
+	def build_tree(self):
 		self.G = {0: []}  # parent to child, int -> list
 		self.H = dict()  # child to parent, int -> int
-		self.nodeCount = 1
-		self.NS = dict()  # node start index
-		self.NE = dict()  # node end index
-		self.SS = dict()  # suffix start index
+		self.node_count = 1
+		self.node_start = dict()  # node start index
+		self.node_end = dict()  # node end index
+		self.suffix_start = dict()  # suffix start index
 		for ix in range(len(self.Text)):
 			# print(ix)
-			self.threadSuffix(ix)
+			self.thread_suffix(ix)
 
-	def exportIndex(self, filename, SA_gap=None, C_gap=None):
+	def export_index(self, filename, suffix_array_gap=None, count_array_gap=None):
 		g = open(filename, 'w')
-		self.SA_gap = SA_gap
-		self.C_gap = C_gap
+		self.suffix_array_gap = suffix_array_gap
+		self.count_gap = count_array_gap
 		g.write(self.BWT + '\n')
-		if SA_gap is None:
+		if suffix_array_gap is None:
 			g.write(','.join(str(i) + ';' + str(s) for i, s in enumerate(self.suffixArray)) + '\n')
 		else:
-			g.write(','.join(str(i) + ';' + str(s) for i, s in enumerate(self.suffixArray) if s % SA_gap == 0) + '\n')
+			g.write(','.join(str(i) + ';' + str(s) for i, s in enumerate(self.suffixArray) if s % suffix_array_gap == 0) + '\n')
 		g.write(','.join(str(x) for x in sorted(self.Count.keys())) + '\n')
 		for x in sorted(self.Count.keys()):
-			if C_gap is None:
+			if count_array_gap is None:
 				g.write(','.join(str(self.Count[x][i]) for i in range(len(self.Count[x]))) + '\n')
 			else:
-				g.write(','.join(str(self.Count[x][i]) for i in range(len(self.Count[x])) if i % C_gap == 0) + '\n')
-		g.write(','.join(str(self.FirstOccurrence[x]) for x in sorted(self.FirstOccurrence.keys())) + '\n')
-		g.write(str(SA_gap) + '\n')
-		g.write(str(C_gap) + '\n')
+				g.write(','.join(str(self.Count[x][i]) for i in range(len(self.Count[x])) if i % count_array_gap == 0) + '\n')
+		g.write(','.join(str(self.first_occurrence[x]) for x in sorted(self.first_occurrence.keys())) + '\n')
+		g.write(str(suffix_array_gap) + '\n')
+		g.write(str(count_array_gap) + '\n')
 
 	def Solve(self):
 		S = [('', 0)]
@@ -72,22 +74,22 @@ class BWIndex():
 			node = S.pop()
 			nodeIx = node[1]
 			if self.G.get(nodeIx) is not None:
-				if self.G[nodeIx] != []:
-					children = [(self.Text[self.NS[child]], child) for child in self.G[nodeIx]]
+				if self.G[nodeIx]:
+					children = [(self.Text[self.node_start[child]], child) for child in self.G[nodeIx]]
 					children.sort(reverse=True)
 					S += children
 			else:
-				transform += self.Text[self.SS[nodeIx] - 1]
-				suffixArray.append(self.SS[nodeIx])
+				transform += self.Text[self.suffix_start[nodeIx] - 1]
+				suffixArray.append(self.suffix_start[nodeIx])
 		self.BWT = transform
 		self.suffixArray = suffixArray
 
 	def drawGraph(self, maxLabelLength=None, outFile=None):
 		D = Digraph()
-		self.NS[0] = 0
-		self.NE[0] = -1
-		for i in range(self.nodeCount):
-			label = self.Text[self.NS[i]:self.NE[i] + 1]
+		self.node_start[0] = 0
+		self.node_end[0] = -1
+		for i in range(self.node_count):
+			label = self.Text[self.node_start[i]:self.node_end[i] + 1]
 			if maxLabelLength:
 				label = label[:maxLabelLength]
 			D.node(str(i), label)
@@ -97,35 +99,35 @@ class BWIndex():
 		D.render(outFile, view=True)
 
 	def report(self):
-		print(self.G, self.H, self.NS, self.NE, self.SS, self.BWT, self.suffixArray, self.FirstOccurrence, self.Count)
+		print(self.G, self.H, self.node_start, self.node_end, self.suffix_start, self.BWT, self.suffixArray, self.first_occurrence, self.Count)
 
 	def appendNode(self, parent, nodeStringStart, suffixStart):
-		newNode = self.nodeCount
+		newNode = self.node_count
 		self.G[parent] = self.G.get(parent, []) + [newNode]
 		self.H[newNode] = parent
-		self.NS[newNode] = nodeStringStart
-		self.NE[newNode] = len(self.Text) - 1
-		self.SS[newNode] = suffixStart
-		self.nodeCount += 1
+		self.node_start[newNode] = nodeStringStart
+		self.node_end[newNode] = len(self.Text) - 1
+		self.suffix_start[newNode] = suffixStart
+		self.node_count += 1
 
 	def splitNode(self, node, index):
 		# introduce a new node whose parent is the original parent of node, make it the new parent of node
-		newNode = self.nodeCount
+		newNode = self.node_count
 		parent = self.H[node]
 		self.G[newNode] = [node]
 		self.G[parent].remove(node)
 		self.G[parent] = self.G.get(parent, []) + [newNode]
 		self.H[newNode] = parent
 		self.H[node] = newNode
-		self.NS[newNode] = self.NS[node]
-		self.NE[newNode] = index - 1
-		self.NS[node] = index
-		self.nodeCount += 1
+		self.node_start[newNode] = self.node_start[node]
+		self.node_end[newNode] = index - 1
+		self.node_start[node] = index
+		self.node_count += 1
 		return newNode
 
 	def findMatchingChild(self, node, loc):
 		for child in self.G[node]:
-			if self.Text[loc] == self.Text[self.NS[child]]:
+			if self.Text[loc] == self.Text[self.node_start[child]]:
 				return child
 		return -1
 
@@ -133,14 +135,14 @@ class BWIndex():
 		offset = 1
 		while True:
 			if ix + offset >= len(self.Text):
-				return self.NS[node] + offset
-			if self.NS[node] + offset > self.NE[node]:
+				return self.node_start[node] + offset
+			if self.node_start[node] + offset > self.node_end[node]:
 				return -1
-			if self.Text[self.NS[node] + offset] != self.Text[ix + offset]:
-				return self.NS[node] + offset
+			if self.Text[self.node_start[node] + offset] != self.Text[ix + offset]:
+				return self.node_start[node] + offset
 			offset += 1
 
-	def threadSuffix(self, ix):
+	def thread_suffix(self, ix):
 		'''
 		look for a node that matches the start of the current substring
 		if none found, create a new node and set its string to the current substring and SS to the initial index
@@ -158,14 +160,14 @@ class BWIndex():
 			node = nextNode
 			splitLoc = self.firstMismatch(node, curIx)
 			if splitLoc == -1:
-				curIx += self.NE[node] - self.NS[node] + 1
+				curIx += self.node_end[node] - self.node_start[node] + 1
 				continue
 			newNode = self.splitNode(node, splitLoc)
-			self.appendNode(newNode, splitLoc + (curIx - self.NS[newNode]), ix)
-			c = self.nodeCount - 1
+			self.appendNode(newNode, splitLoc + (curIx - self.node_start[newNode]), ix)
+			c = self.node_count - 1
 			break
 
-	def computeCountArray(self):
+	def compute_count_array(self):
 		for i in self.alphabet:
 			self.Count[i] = [0] * (len(self.Text) + 1)  # check does this cause any reference issues?
 		for j in range(1, len(self.BWT) + 1):
@@ -183,14 +185,14 @@ class BWIndex():
 				symbol = Pattern[-1]
 				Pattern = Pattern[:-1]
 				if self.Count[symbol][bottom + 1] - self.Count[symbol][top] > 0:
-					top = self.FirstOccurrence[symbol] + self.Count[symbol][top]
-					bottom = self.FirstOccurrence[symbol] + self.Count[symbol][bottom + 1] - 1
+					top = self.first_occurrence[symbol] + self.Count[symbol][top]
+					bottom = self.first_occurrence[symbol] + self.Count[symbol][bottom + 1] - 1
 				else:
 					return 0
 			else:
 				return bottom - top + 1
 
-	def loadGenomeFromFile(self, genomefile):
+	def load_genome_from_file(self, genomefile):
 		lines = []
 		with open(genomefile) as f:
 			for line in f:
@@ -236,4 +238,4 @@ if __name__ == '__main__':
 		BWI = BWIndex(genome=args.genome)
 	elif args.genomefile:
 		BWI = BWIndex(genomefile=args.genomefile)
-	BWI.exportIndex(args.indexfile, C_gap=args.countgap, SA_gap=args.suffixgap)
+	BWI.export_index(args.indexfile, count_array_gap=args.countgap, suffix_array_gap=args.suffixgap)
